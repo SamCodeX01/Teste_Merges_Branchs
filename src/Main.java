da1b9bdb1c5f7e9251ccd56d4ec32f7b54a66042
SamCodeX01 <txtstring@gmail.com>
2025-05-20 17:20:44 -0300

    Adicionei um Scanner

--- src/Main.java
+++ src/Main.java
@@ -1,8 +1,14 @@
+import java.util.Scanner;
+
 public class Main {
     public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
 
         System.out.println("Hello, World!");
 
         String[] formulario = new String[3];
+
+        System.out.print("Nome:");
+        formulario[0] = sc.nextLine();
     }
 }
